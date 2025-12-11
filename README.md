061c296ebad50e4c94c4e4cb3f41b36ee84a4172
Vinicius Santos <viniciussantos17178007@gmail.com>
2025-12-10 22:50:12 -0300

    exercicios_preparativos
    feito para melhorar pessoais

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Exercicios_python
-Projects developments in python
+Resolução de questões da OBMEP (Olimpiada Brasileira de Matemática das Escolas Públicas) exercitando a lógica de programação e a própria linguagem.
+---
+## Metodologia:
+primeiro o código com ex00_.py é o problema sendo resolvido por IA, dessa forma obtendo uma direção a ser seguida para assim criar meu próprio código sendo o notes00_.py
+---
+## Disclaimer:
+O código feito por IA é _APENAS_ para fins de comparação, em nenhum momento foi copiado qualquer linha de código ou usado a lógica do código criado por inteligência artificial. Logo após a criação do código, foi submetido a avaliação do Gemini e assim futuramente serão feitas atualizações a medida que ocorrer a evolução do programador.
+
---
+++ notes001.py
@@ -0,0 +1,23 @@
+''' Pedro tem muitas notas de 2 e de 5 reais. Ele precisa pagar uma conta de 20 reais. De quantas maneiras diferentes Pedro pode pagar essa conta sem receber troco?'''
+# Feito por mim
+
+def maneiras_de_pagar(valor_maximo, nota1, nota2):
+
+    possibilidades = []
+    for valor in range(valor_maximo, 0, -1):
+        
+        if nota1*valor == valor_maximo:
+            possibilidades.append(valor)
+        
+        if nota2*valor == valor_maximo:
+            possibilidades.append(valor)
+            
+    for valor in range(valor_maximo, 0, -1):
+        for valor2 in range(valor_maximo, 0, -1):
+            if nota1*valor+nota2*valor2 == valor_maximo:
+                possibilidades.append(20)
+        
+        
+    print(f'Com as notas de R${nota1},00 e R${nota2},00 existem {len(possibilidades)} possibilidades de combinação.')
+    
+print(maneiras_de_pagar(50,2,5))
